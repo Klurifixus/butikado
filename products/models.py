15bc329a7aa6eb8e7f56f36497dcc3fbc911253f
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2024-01-03 14:41:31 +0100

    added quantity +/- possibility to shoping bag

--- products/models.py
+++ products/models.py
@@ -6,8 +6,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
         
-    name = models.CharField(max_length=255)
-    friendly_name = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -18,8 +18,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=255, null=True, blank=True)
-    name = models.CharField(max_length=255)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
