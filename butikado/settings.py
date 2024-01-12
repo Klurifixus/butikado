df41d41e72a9629cd1b27a7a7048f4a12d56de82
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2024-01-12 17:05:23 +0100

    uppdated settings.py with new secretkey

--- butikado/settings.py
+++ butikado/settings.py
@@ -18,11 +18,10 @@ from .env import DEBUG, SECRET_KEY, STRIPE_PUBLIC_KEY, STRIPE_SECRET_KEY#, STRIP
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@atgequp@n+2drf9gv_jn98)17)0^-#hbxyuc6a=xu9xq&d-0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
