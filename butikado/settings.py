e8c290d35ac295b6252026db0cc03b648809abea
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2024-01-24 12:48:42 +0100

    #8 changing allowed host setting

--- butikado/settings.py
+++ butikado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'earnsshopskater-b0a8c0e4297a.herokuapp.com']
 
 # Application definition
 
