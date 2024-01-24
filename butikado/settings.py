6225e2b8404afd52ff48b150fb14d58b202f47cf
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2024-01-24 20:25:41 +0100

    update for default from email

--- butikado/settings.py
+++ butikado/settings.py
@@ -117,11 +117,12 @@ try:
 except ValueError:
     EMAIL_PORT = 587
 
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'False').lower() == 'true'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False').lower() == 'true'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'pirrefixus@gmail.com')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 # Allauth Account Settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
