ace28b6acbcdaa76682cf74af0f86b8ce1f997c4
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2024-01-24 14:33:16 +0100

    #16 set up js buildpack to heroku adjust whitenoise in wsgi and settings.py

--- butikado/wsgi.py
+++ butikado/wsgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
+from django.conf import settings
 
 load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'butikado.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.MEDIA_ROOT)
+application.add_files(os.path.join(settings.BASE_DIR, 'media'), prefix='media/')
