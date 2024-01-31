37e265dfda6391aef4c6d12929b8b4b6d30ff302
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2024-01-31 20:36:12 +0100

    #28

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,14 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-
     path('', views.blog_home, name='blog_home'),
     path('skate/', views.skate_section, name='skate_section'),
     path('music_section/', views.music_section, name='music_section'),
     path('history_section/', views.history_section, name='history_section'),
     path('add/', views.add_blog_post, name='add_blog_post'),
-    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('like_dislike/', views.like_dislike, name='like_dislike'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
